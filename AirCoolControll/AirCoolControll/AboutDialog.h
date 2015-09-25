7558500e2fd98c4126e5a5ba9606e802b9238ca5
alexandr <alexandr.pryadka@gmail.com>
2015-09-26 00:17:13 +0300

    обновлен проекта после неправильного мержа. модифицирован эбаут класс

---
+++ AirCoolControll/AirCoolControll/AboutDialog.h
@@ -0,0 +1,20 @@
+#ifndef ABOUTDIALOG_H
+#define ABOUTDIALOG_H
+
+#include <QDialog>
+#include "ui_AboutDialog.h"
+
+class AboutDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    AboutDialog(QWidget *parent = 0);
+    ~AboutDialog();
+
+private slots:
+private:
+    Ui::AboutDialog ui;
+};
+
+#endif // ABOUTDIALOG_H
